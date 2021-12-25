8e4ef5c48cbb7f653e658d67de90bd3d277a20bc
avi.13 <avicekbhatta.123@gmail.com>
2021-12-25 11:26:40 +0545

    python

---
+++ Tuple/1.py
@@ -0,0 +1,5 @@
+# Write a Python program to add an item in a tuple.
+tup=(5,1,10)
+print(tup)
+tup= tup+(5,)
+print(tup)
---
+++ list/5.py
@@ -0,0 +1,5 @@
+# Write a Python program to print a specified list after removing the 4th elements.
+a = [10,20,30,4,2]
+print(a)
+a.remove(4)
+print(a)
