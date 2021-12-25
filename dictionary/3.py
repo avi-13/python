1aa01871047263d63c6e414772688263040ea588
avi.13 <avicekbhatta.123@gmail.com>
2021-12-25 18:01:53 +0545

    python

--- dictionary/3.py
+++ dictionary/3.py
@@ -1,4 +1,6 @@
 #WAP to print a dictionary where the keys are numbers between 1 and 15 (both included) 
 # and the values are square of keys.
-a={1:1,2:4,3:9,4:16,5:25,6:36,7:49,8:8**2,9:9**2,10:100,11:11**2,12:12**2,13:2**13,14:14**2,15:15**2}
-print(a)
+d=dict()
+for x in range(1,16):
+    d[x]=x**2
+print(d)
